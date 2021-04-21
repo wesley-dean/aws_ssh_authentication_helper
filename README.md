6688c39c9eddfc13c69ebd2668a4eeaa5d8c38c9
Wes Dean <>
2021-04-21 13:29:21 -0400

    Update doc generator, link markdown

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
+
 aws_ssh_authentication_helper.bash
-==========
+==================================
 
 Overview
-----------
+--------
 
 allow users to authenticate via SSH public keys in AWS Code Commit
 
@@ -24,29 +25,47 @@ saved to users ~/.ssh/authorized_keys).
 
 Additionally, when the user attempts to connect, an AWS IAM group may be
 provided; if so, the tool will verify that the user exists in that group
-before permitting authentication.
+before permitting authentication.  The group to query may be specified
+via '-g' followed by the name of the AWS IAM group.
+
+User Creation
+-------------
 
 When users attempt to connect, this tool can create their accounts
 automatically.  Accounts will only be created if the username matches
 an AWS IAM user.  Moreover, if an AWS IAM group is provided, they will
 only be created if that AWS IAM user exists in that AWS IAM group.
 
+User creation is disabled by default but may be enabled via '-u'
+
+Local Groups
+------------
+
 The tool also supports group management in that when users attempt to
 authenticate, they can be automatically added to a local (non-IAM) group.
-If the group doesn't exist, it will be created.
+If the group doesn't exist, it will be created.  This functionality
+is disabled by default but may be enabled via '-m' while the name
+of the local group to manage may be specified via '-l'
 
 The adding of users to groups, while it takes place at authentication
 time, is not restricted to only new users being created for the first
 time.  That is, if the manage group setting is enabled and a previously
 seen user attempts to login, they will be added to that group.
 
+User Removal
+------------
+
 When a user attempts to authenticate and either they do not belong to
 the required AWS IAM group or there is no corresponding AWS IAM user
-account, their local account can be removed.
+account, their local account can be removed.  Account removal
+functionality is disabled by default but may be enabled via '-r'
 
 Regardless of the remove user setting, users who do not have an
 enabled AWS IAM user account or are not in a specified
 AWS IAM group will be denied authentication.
+
+AWS IAM Access
+--------------
 
 It's notable that changes made in AWS only affect new connections;
 that is, if a user is already logged in and then their AWS IAM
@@ -54,30 +73,92 @@ account is disabled, their connection won't be dropped, interrupted,
 etc..  Manual intervention would be required to terminate current,
 active sessions.
 
-Author
+The following AWS IAM permissions are required for this script to
+interact with AWS's APIs:
+
+* iam:GetSSHPublicKey
+* iam:ListSSHPublicKey
+* iam:ListGroupsForUser
+* iam:GetGroup
+* iam:GetUser
+
+Consider the following AWS IAM policy:
+
+```json
+
+{
+ "Version": "2012-10-17",
+ "Statement" : [
+   {
+     "Sid": "IAMUserSSHKeys",
+     "Effect" : "Allow",
+     "Action" : [
+       "iam:GetSSHPublicKey",
+       "iam:ListSSHPublicKey",
+       "iam:ListGroupsForUser",
+       "iam:GetGroup",
+       "iam:GetUser"
+     ],
+     "Resource" : [
+       "*"
+     ]
+   }
+ ]
+}
+
+```
+
+SSHD setup
 ----------
+
+The SSH daemon needs to be configured to use this script to acquire
+the "authorized_keys" files (i.e., the list of public portions of
+SSH keys that are allowed access).  This can be done by setting the
+`AuthorizedKeysCommand` in `/etc/ssh/sshd_config` as follows:
+
+```
+
+AuthorizedKeysCommand /path/to/aws_ssh_authentication_helper.bash
+AuthorizedKeysCommandUser nobody
+
+```
+
+Note: either `nobody` or `root` should be used depending on whether
+or not this script will be managing users or groups (i.e., creating
+new users, adding users to groups, removing users, etc.) or not.  If
+the script will be managing users or groups, then `root` needs to be
+used.  If, however, accounts and groups will be managed manually,
+then `nobody` may be used.  Using `nobody` is much more secure;
+however, it also requires manual work to create accounts and such.
+
+Author
+------
+
 Wes Dean <wdean@flexion.us>
 
 Parameters
 ----------
 
-*  -c : create local group
-*  -g : the remote group to query
-*  -h : show help text
-*  -m : mange local group members
-*  -r : remove local user
-*  -u : create local user
+* -c : create local group
+* -g : the remote group to query
+* -h : show help text
+* -l : the local group to create
+* -m : mange local group members
+* -r : remove local user
+* -u : create local user
 
 Defaults
-----------
+--------
 
-*  CREATE_USER:  default for whether to create local users
-   (default: "false")
-*  REMOVE_USER:  default for whether to remove local users
-   (default: "false")
-*  CREATE_GROUP:  default for whether to create local groups
-   (default: "false")
-*  MANAGE_GROUP:  default for whether to manage local group members
-   (default: "false")
-*  REMOTE_GROUP:  defaut remote group to query
-   (default: "sshusers")
+* CREATE_USER:  default for whether to create local users
+  (default: "false")
+* REMOVE_USER:  default for whether to remove local users
+  (default: "false")
+* CREATE_GROUP:  default for whether to create local groups
+  (default: "false")
+* MANAGE_GROUP:  default for whether to manage local group members
+  (default: "false")
+* REMOTE_GROUP:  defaut remote group to query
+  (default: "")
+* LOCAL_GROUP:  default local group to manage
+  (default: "users")
--- aws_ssh_authentication_helper.bash
+++ aws_ssh_authentication_helper.bash
@@ -21,35 +21,111 @@
 ##
 ## Additionally, when the user attempts to connect, an AWS IAM group may be
 ## provided; if so, the tool will verify that the user exists in that group
-## before permitting authentication.
+## before permitting authentication.  The group to query may be specified
+## via '-g' followed by the name of the AWS IAM group.
+##
+## User Creation
+## -------------
 ##
 ## When users attempt to connect, this tool can create their accounts
 ## automatically.  Accounts will only be created if the username matches
 ## an AWS IAM user.  Moreover, if an AWS IAM group is provided, they will
 ## only be created if that AWS IAM user exists in that AWS IAM group.
 ##
+## User creation is disabled by default but may be enabled via '-u'
+##
+## Local Groups
+## ------------
+##
 ## The tool also supports group management in that when users attempt to
 ## authenticate, they can be automatically added to a local (non-IAM) group.
-## If the group doesn't exist, it will be created.
+## If the group doesn't exist, it will be created.  This functionality
+## is disabled by default but may be enabled via '-m' while the name
+## of the local group to manage may be specified via '-l'
 ##
 ## The adding of users to groups, while it takes place at authentication
 ## time, is not restricted to only new users being created for the first
 ## time.  That is, if the manage group setting is enabled and a previously
 ## seen user attempts to login, they will be added to that group.
 ##
+## User Removal
+## ------------
+##
 ## When a user attempts to authenticate and either they do not belong to
 ## the required AWS IAM group or there is no corresponding AWS IAM user
-## account, their local account can be removed.
+## account, their local account can be removed.  Account removal
+## functionality is disabled by default but may be enabled via '-r'
 ##
 ## Regardless of the remove user setting, users who do not have an
 ## enabled AWS IAM user account or are not in a specified
 ## AWS IAM group will be denied authentication.
+##
+## AWS IAM Access
+## --------------
 ##
 ## It's notable that changes made in AWS only affect new connections;
 ## that is, if a user is already logged in and then their AWS IAM
 ## account is disabled, their connection won't be dropped, interrupted,
 ## etc..  Manual intervention would be required to terminate current,
 ## active sessions.
+##
+## The following AWS IAM permissions are required for this script to
+## interact with AWS's APIs:
+##
+## * iam:GetSSHPublicKey
+## * iam:ListSSHPublicKey
+## * iam:ListGroupsForUser
+## * iam:GetGroup
+## * iam:GetUser
+##
+## Consider the following AWS IAM policy:
+##
+## ```json
+##
+## {
+##  "Version": "2012-10-17",
+##  "Statement" : [
+##    {
+##      "Sid": "IAMUserSSHKeys",
+##      "Effect" : "Allow",
+##      "Action" : [
+##        "iam:GetSSHPublicKey",
+##        "iam:ListSSHPublicKey",
+##        "iam:ListGroupsForUser",
+##        "iam:GetGroup",
+##        "iam:GetUser"
+##      ],
+##      "Resource" : [
+##        "*"
+##      ]
+##    }
+##  ]
+## }
+##
+## ```
+##
+## SSHD setup
+## ----------
+##
+## The SSH daemon needs to be configured to use this script to acquire
+## the "authorized_keys" files (i.e., the list of public portions of
+## SSH keys that are allowed access).  This can be done by setting the
+## `AuthorizedKeysCommand` in `/etc/ssh/sshd_config` as follows:
+##
+## ```
+##
+## AuthorizedKeysCommand /path/to/aws_ssh_authentication_helper.bash
+## AuthorizedKeysCommandUser nobody
+##
+## ```
+##
+## Note: either `nobody` or `root` should be used depending on whether
+## or not this script will be managing users or groups (i.e., creating
+## new users, adding users to groups, removing users, etc.) or not.  If
+## the script will be managing users or groups, then `root` needs to be
+## used.  If, however, accounts and groups will be managed manually,
+## then `nobody` may be used.  Using `nobody` is much more secure;
+## however, it also requires manual work to create accounts and such.
 ##
 ## @author Wes Dean <wdean@flexion.us>
 
@@ -67,7 +143,10 @@ DEFAULT_CREATE_GROUP="false"
 DEFAULT_MANAGE_GROUP="false"
 
 ## @var DEFAULT_REMOTE_GROUP defaut remote group to query
-DEFAULT_REMOTE_GROUP="sshusers"
+DEFAULT_REMOTE_GROUP=""
+
+## @var DEFAULT_LOCAL_GROUP default local group to manage
+DEFAULT_LOCAL_GROUP="users"
 
 
 ## @fn is_true()
@@ -143,6 +222,7 @@ md_header() {
   for n in $(seq 1 "${#header}") ; do
     echo -n "$character"
   done
+  echo ""
   echo ""
 }
 
@@ -430,7 +510,7 @@ check_remote_group() {
 check_remote_user() {
   username="${1?No username provided}"
 
-  user_data="$(aws iam list-users --query "Users[?UserName=='$username'].Arn" --output text)"
+  user_data="$(aws iam get-user --user-name "$username" --output text)"
 
   if [[ "$user_data" =~ arn:aws ]] ; then
     return 0
@@ -494,10 +574,10 @@ show_help() {
   sed \
     --zero-terminated \
     --regexp-extended \
-    --expression='s/.*@[Bb]rief *(.*)@[Aa]uthor *([^\n]*).*/\nOverview\n--------\n\n\1Author\n------\n\2\n\n/' \
-    --regexp-extended \
-    --expression='s/\B@[a-z]* *//g' \
-    --expression 's/## *//g' \
+    --expression='s/.*@[Bb]rief *(.*)@[Aa]uthor *([^\n]*).*/Overview\n--------\n\n\1Author\n------\n\n\2\n/' \
+    --regexp-extended --expression='s/\B@[a-z]* *//g' \
+    --regexp-extended --expression='s/##[^ ]/\n/g' \
+    --regexp-extended --expression 's/## //g' \
     "$0"
 
   md_header "Parameters" "-"
@@ -505,7 +585,7 @@ show_help() {
  sed \
    --quiet \
    --regexp-extended \
-   --expression='s/^ *([A-Z]) * \).*#{2}- */*  -\1 : /ip' \
+   --expression='s/^ *([A-Z]) * \).*#{2}- */* -\1 : /ip' \
    "$0" \
    | sort --ignore-case
 
@@ -516,10 +596,10 @@ show_help() {
     --quiet \
     --expression='
       /^[[:space:]]*##[[:space:]]*@var/ {
-         s/^[[:space:]]*##[[:space:]]*@var[[:space:]]*((DEFAULT_)?([^[:space:]]+))(.*)/*  \3: \4/;
+         s/^[[:space:]]*##[[:space:]]*@var[[:space:]]*((DEFAULT_)?([^[:space:]]+))(.*)/* \3: \4/;
          p;
          n;
-         s/([^=]*=)(.*)$/   (default: \2)/;
+         s/([^=]*=)(.*)$/  (default: \2)/;
          p;
       }' \
     "$0"
@@ -531,13 +611,15 @@ remove_user="$(is_true "$DEFAULT_REMOVE_USER")"   # -r
 create_group="$(is_true "$DEFAULT_CREATE_GROUP")" # -c
 manage_group="$(is_true "$DEFAULT_MANAGE_GROUP")" # -m
 remote_group="$DEFAULT_REMOTE_GROUP"              # -g
+local_group="$DEFAULT_LOCAL_GROUP"                # -l
 
-while getopts "urcmg:h" option ; do
+while getopts "urcmg:l:h" option ; do
   case "$option" in
     u ) create_user=$((1 - create_user)) ;; ##- create local user
     r ) remove_user=$((1 - remove_user)) ;; ##- remove local user
     h ) show_help ; exit 0 ;; ##- show help text
     g ) remote_group="$OPTARG" ;; ##- the remote group to query
+    l ) local_group="$OPTARG" ;; ##- the local group to create
     c ) create_group=$((1 - create_group)) ;; ##- create local group
     m ) manage_group=$((1 - manage_group)) ;; ##- mange local group members
     * ) echo "Invalid option '$option'" 1>&2 ; show_help 1>&2 ; exit 1 ;;
@@ -553,7 +635,8 @@ if ! numeric_uid="$(name_to_id "$username" "passwd")" ; then
   exit 1
 fi
 
-if ! numeric_gid="$(name_to_id "$local_group" "group")" ; then
+if [ -n "$local_group" ] \
+&& ! numeric_gid="$(name_to_id "$local_group" "group")" ; then
   logger is "Could not find an appropriate GID for '$local_group'"
   exit 1
 fi
